80c4dd834874fb3676bc95b060a35d8137398085
EternalVigil <theeternalvigil@gmail.com>
2016-10-31 18:53:50 -0700

    HTML structure and JS additions

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,1 +1,36 @@
 // JavaScript Document
+var apiURL = "http://api.giphy.com/v1/gifs/search?q=";
+var apiKey = "&api_key=dc6zaTOxFJmzC";
+var searchList = [];
+
+
+$("#submitButton").on("click", function(){
+var searchTerm = $("#searchKey").val().trim();	
+console.log(searchTerm);
+});
+
+for (var i = 0; i < searchList.length; i++){
+	var b = $("<button/>");
+	b.text(searchList[i]);
+	b.attr("class", "itemButton");
+	b.val(searchList[i]);
+}
+
+
+
+//var searchTerm = "monkey";
+// var queryURL = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
+	
+	var queryURL = apiURL + searchTerm + apiKey;
+
+$.ajax({
+	url: queryURL,
+	method: "GET"
+	
+}).done(function(data){
+	console.log(data);
+});
+
+
+var imageHolder = $("<div/>");
+var giphyImage = $("<img/>");
