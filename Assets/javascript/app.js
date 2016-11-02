91f4c8fe184fdbfe4a06bd06461ac3eb97cf5bd1
Brandon Vigil <brandonvigil@Brandons-MacBook-Pro.local>
2016-11-01 17:55:15 -0700

    search functiction working + js additions

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -2,34 +2,39 @@
 var apiURL = "http://api.giphy.com/v1/gifs/search?q=";
 var apiKey = "&api_key=dc6zaTOxFJmzC";
 var searchList = [];
+var searchTerm = "";
+var queryURL = "";
 
-
-$("#submitButton").on("click", function(){
-var searchTerm = $("#searchKey").val().trim();	
-console.log(searchTerm);
+$("#submit").on("click", function () {
+	searchTerm = $("#searchWord").val().trim();
+	console.log(searchTerm);
 	searchList.push(searchTerm);
+	console.log(searchList);
+	$("#searchWord").val("");
+	queryURL = apiURL + searchTerm + apiKey;
+	console.log(queryURL);
+	return false;
 });
 
-for (var i = 0; i < searchList.length; i++){
+for (var i = 0; i < searchList.length; i++) {
+	console.log("loop time");
 	var b = $("<button/>");
 	b.text(searchList[i]);
 	b.attr("class", "itemButton");
 	b.attr("data-name", searchList[i]);
+	$("#typeRow").append(b);
 }
 
-
-
-//var searchTerm = "monkey";
 // var queryURL = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
-	
-	var queryURL = apiURL + searchTerm + apiKey;
+
+
 
 $.ajax({
 	url: queryURL,
 	method: "GET"
-	
-}).done(function(data){
-	console.log(data);
+
+}).done(function (data) {
+	//console.log(data);
 });
 
 
